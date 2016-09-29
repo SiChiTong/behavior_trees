4db875c14321e16e9fb96400d8573e8b0f1ce870
fsuarez6 <fsuarez6@gmail.com>
2016-09-29 16:59:16 +0800

    Change to D3js based viewer

--- behave_viewer/scripts/example_dotcode.py
+++ behave_viewer/scripts/example_dotcode.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python
 import rospy
 import anyjson
-import numpy as np
+import itertools
 import networkx as nx
-import pygraphviz as pgv
+from networkx.readwrite import json_graph
 from std_msgs.msg import String
 
 # Initialize ROS publisher
 rospy.init_node('example_dotcode')
 pub = rospy.Publisher('/dotcode', String, queue_size=3)
 rate = rospy.Rate(0.5)
+graphs = []
+graphs.append( nx.DiGraph([(1,2),(1,3),(1,4),(2,5),(2,6)]) )
+graphs.append( nx.DiGraph([(1,2),(1,3),(1,4),(4,5),(4,6),(2,7),(2,8)]) )
+graphs.append( nx.DiGraph([(1,2),(1,3),(2,4),(3,5),(4,6)]) )
+cycle = itertools.cycle(graphs)
 rospy.loginfo('Publishing DOT graph to: /dotcode')
 while not rospy.is_shutdown():
-  # Create random graphs
-  G = nx.gn_graph(10)
-  # Random shapes and colors
-  for nodeid in G.nodes():
-    shape = ['circle','diamond','rectangle'][np.random.randint(0,3)]
-    color = ['red','green','orange','white'][np.random.randint(0,4)]
-    G.node[nodeid] = {'label': str(nodeid), 'shape':shape, 'color':color}
-  for edge in G.edges():
-    color = ['red','green','orange','black'][np.random.randint(0,4)]
-    G.edge[edge[0]][edge[1]] = {'color':color}
-  # Serialize and publish the graph
-  A = nx.nx_agraph.to_agraph(G)
-  pub.publish(data=A.to_string())
+  G = cycle.next()
+  for n in G:
+    G.node[n]['name'] = n
+  data = json_graph.tree_data(G, root=1)
+  pub.publish(data=str(anyjson.dumps(data)))
   rate.sleep()
 
 
